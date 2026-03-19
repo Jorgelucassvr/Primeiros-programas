17c79fef9edf6a2b4093e54d6391617ff5c5df47
Jorge lucas vieira <jorgelucasvieira18@gmail.com>
2026-03-19 18:12:34 -0300

    mais exercicios

---
+++ 4 Exercicios/sequencia de fibonacci.py
@@ -0,0 +1,11 @@
+print("Sequencia de fibonacci")
+contador = int(input("Iinsira o numero de algarismos voce quer: "))
+A = 0
+B = 1
+while contador >= 0:
+    print(f"{A}") 
+
+    proximo = A + B
+    A = B
+    B = proximo 
+    contador = contador - 1
